1304444d1b3b557e1e4b2e06166bb6c16fdd555f
Rudolfhf <rudolf-hoffmann@hotmail.com>
2026-06-02 00:31:23 -0300

    retorno corrigido e funcoes corrigidas

--- funcoes.py
+++ funcoes.py
@@ -5,9 +5,10 @@ from datetime import datetime
 products = 'arquivos\olist_products_dataset.csv'
 orders = 'arquivos\olist_orders_dataset.csv'
 
+
 dimencoes = ['product_weight_g', 'product_length_cm', 'product_height_cm', 'product_width_cm']
 
-def tirarMediana(arquivo, nomecategoria, campos_vazios):
+def calcularMedia(arquivo, nomecategoria, campos_vazios):
     with open(arquivo, "r", newline='', encoding='utf-8') as csvfile:
         leitor = csv.DictReader(csvfile, delimiter=',') 
         #mediana = sum(leitor['product_weight_g'])
@@ -15,17 +16,15 @@ def tirarMediana(arquivo, nomecategoria, campos_vazios):
         quantidade = 0
         soma = {campo: 0 for campo in campos_vazios}
         quantidade = {campo: 0 for campo in campos_vazios}
+        medias = {}
         print(f"Iniciando Calculo da soma para cada dimensão da categoria:{nomecategoria}")
-        for linha in leitor:        
+        for linha in leitor:                   
             if linha['product_category_name'] == nomecategoria:
-                
-                for campo in campos_vazios:
-                    
+                  for campo in campos_vazios:                  
                     if linha[campo] not in ('', None):
                         soma[campo] += int(linha[campo])
                         quantidade[campo] += 1
-                 
-                              
+                                              
         print(f"Categoria: {nomecategoria}")
         print(f"Soma Total: {soma}")
         print(f"Quantidade: {quantidade}")
@@ -36,28 +35,38 @@ def tirarMediana(arquivo, nomecategoria, campos_vazios):
 
             if quantidade[campo] > 0:
                 media = soma[campo] / quantidade[campo]
+                medias[campo] = media
                 print(f"{campo}: {media}")
-                #return media
-                
+                               
             else:
-                print(f"{campo}: sem dados")    
+                print(f"{campo}: sem dados") 
+                return print("sem dados")   
+        print("==========================================")
+        return medias
+        
 
 
 def funcValidar_tratar(arquivo):
+
+    total_linhas_processadas = 0
+    total_nulos_corrigidos = 0
     #Contadores
     qtd_product_nulos = 0
     linhas_descartadas = []
     #dimencoes = ['product_weight_g', 'product_length_cm', 'product_height_cm', 'product_width_cm']
     linhas_sem_dimensoes = []
     campos_vazios = []
-    
+    resultados = []
     with open(arquivo, "r", newline='', encoding='utf-8') as csvfile:
         leitor = csv.DictReader(csvfile, delimiter=',')     
         
-        for linha in leitor:  
+        for linha in leitor: 
+            total_linhas_processadas += 1 
             #Correção dos nulos e vazius na coluna "product_category_name" por "Sem Categoria"                           
             if linha and not linha['product_category_name']:             
                 linha['product_category_name'] = "Sem Categoria"
+                total_nulos_corrigidos += 1
+            
                               
             #Verificar quantidade de nulos nas dimensões:
             #linhas_sem_dimencoes = list(filter(lambda campo: not linha[campo] and linha['product_category_name'] != 'Sem Categoria', dimencoes))
@@ -66,40 +75,34 @@ def funcValidar_tratar(arquivo):
 
             
             if campos_vazios and linha['product_category_name'] != "Sem Categoria":
+                
                 linhas_sem_dimensoes.append(linha) 
-                print("============================")
+                print("=============================================")
                 print("Novo Produto com dimensoes vazias identificado\n")
                 print(f"ID:{linha['product_id']}\n Categoria:{linha['product_category_name']}\n Tratar:{campos_vazios} ")
-                print("============================")
-                for linha in linhas_sem_dimensoes:
-                    cat = linha['product_category_name']
+                print("---------------------------------------------")
+                #for linha in linhas_sem_dimensoes:
+                cat = linha['product_category_name']
                         
-                tirarMediana(arquivo, cat, campos_vazios) 
+                resultado = calcularMedia(arquivo, cat, campos_vazios)
 
-
-        print(f"Quantidade de linhas sem dimensoes: {len(linhas_sem_dimensoes)}")
-
-        '''
-            if not linha['product_weight_g'] or not linha['product_length_cm'] or not linha['product_height_cm'] or not linha['product_width_cm']:
-                qtd_product_nulos += 1             
-                print(linha)
-                if linha['product_category_name'] == 'Sem Categoria':
-                    print("A linha será descartada, pois não há categoria e nem dimensões")
-                    linhas_descartadas.append(linha['product_id'])
-                #elif linha['product_category_name'] ison linha['product_category_name'].products
-                else:
-                    print(f"O produto {linha['product_category_name']} cujo ID: {linha['product_id']} deve receber a mediana dos demais semelhantes")
+                for campo, media in resultado.items():
+                    linha[campo] = round(media, 2)                   
+                    total_nulos_corrigidos += 1
+                print(f"TESTE FINAL SE FOI CORRIGIDO === > {linha}")
+                
+                resultados.append(resultado)
             
-        print("RELATORIO")
-        print(f"Quantidade de Nulos Identificado nas colunas de dimensões: = {qtd_product_nulos}")
-        print(f"Linhas que serão Descartadas por produto ID {linhas_descartadas}")
+        #print(f"Quantidade de linhas sem dimensoes: {len(linhas_sem_dimensoes)}") 
+                 
+        return resultados, total_linhas_processadas, total_nulos_corrigidos
+        
         '''
-
-        '''with open(arquivo, 'x', encoding='utf-8') as csvfile:
+            with open(arquivo, 'x', encoding='utf-8') as csvfile:
             print(aaa)
        
             salvar = csv.DictWriter(csvfile
-            '''
+        '''
 
 def funcPadronizar_regex(arquivo):
     with open(arquivo, "r", encoding='utf-8') as csvfile:
@@ -165,11 +168,13 @@ def funcRegranegocio_cancelados(arquivo_dois):
                 else:
                     print("N IDENTIFICADO")
                     print(linha['order_status'])
+        
                       
         print("Resultado Regra de Negócio")
         print(f"Quantidade de datas de entregas vazias:{contador_vazios} com seus respectivos valores na coluna order_delivered_customer_date:")
         #print(f"Ordens Canceladas:{order_canceled}, Ordens Enviadas:{order_shipped}, unavailable:{order_unavailable}")
         print(f"{ordersType}")
+        return ordersType['order_canceled']
 
 
 #Função para converter para formato brasileiro dd/mm/aa
@@ -198,18 +203,11 @@ def funcFormatarData(arquivo):
                     #print(f"modificação feita com sucesso")
             print(f"Quantidade de linhas nulas {len(qtd_nulos)}")
                 
-            
-
-
 def relatorioFinal():
     print("PROGRAMA SANITIZADO COM SUCESSO:")
     print("RELATORIO FINAL")
        
-       
-                
-
-
-                     
+          
 #validacao_Ausentes(products)      
 #limpar_padronizar(products)
 #filtrar(orders)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from funcoes import tirarMediana, funcValidar_tratar, funcPadronizar_regex, funcRegranegocio_cancelados, funcFormatarData, relatorioFinal
+from funcoes import calcularMedia, funcValidar_tratar, funcPadronizar_regex, funcRegranegocio_cancelados, funcFormatarData, relatorioFinal
 
 products = 'arquivos\olist_products_dataset.csv'
 orders = 'arquivos\olist_orders_dataset.csv'
@@ -6,10 +6,21 @@ orders = 'arquivos\olist_orders_dataset.csv'
 
 
 
-#print("A sanitização dos arquivos será realizad, O Programa será Executado")
 
-funcValidar_tratar(products)
-#tirarMediana(products, "bebes", ['product_weight_g', 'product_length_cm', 'product_height_cm', 'product_width_cm'])
+print("\n" + 5 * "//===//==")
+print("Iniciando sanitização e tratamento na lista:")
+
+
+listaProdutos, total_linhas_processadas, total_nulos_corrigidos = funcValidar_tratar(products)
+pedidosCancelados = funcRegranegocio_cancelados(orders)
+
+print("\n" + 5 * "===//===")
+print(2 * "//===//" + " RELATORIO " + 2 * "//===//")
+print(5 * "===//===")
+print(f"Total de linhas processadas: {total_linhas_processadas}")
+print(f"Total de registros nulos corrigidos: {total_nulos_corrigidos}")
+print(f"Total de pedidos nulos cancelados: {pedidosCancelados}")
+#calcularMedia(products, "bebes", ['product_weight_g', 'product_length_cm', 'product_height_cm', 'product_width_cm'])
 #funcPadronizar_regex(products)
 #funcRegranegocio_cancelados(orders)
 #funcFormatarData(orders)
